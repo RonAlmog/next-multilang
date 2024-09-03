bbe5e41e69f7bde8e2206ee0e037a12ad885d43d
Ron Almog <ron8132@gmail.com>
2024-09-03 09:50:07 -0400

    add lang switcher

--- app/[lng]/components/footer.tsx
+++ app/[lng]/components/footer.tsx
@@ -8,7 +8,23 @@ type Props = { lng: string };
 const Footer = async ({ lng }: Props) => {
   const { t } = await useTranslation(lng, "footer");
 
-  return <div>Footer</div>;
+  return (
+    <footer className="mt-10">
+      <Trans i18nKey="languageSwitcher" t={t}>
+        Switch from <strong>{lng}</strong> to:{" "}
+      </Trans>
+      {languages
+        .filter((l) => lng !== l)
+        .map((l, index) => {
+          return (
+            <span key={l}>
+              {index > 0 && " or "}
+              <Link href={`/${l}`}>{l}</Link>
+            </span>
+          );
+        })}
+    </footer>
+  );
 };
 
 export default Footer;
