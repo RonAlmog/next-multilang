46d36c0234143f3bd43209b37dcb2cd7b0e5fa5d
Ron Almog <ron8132@gmail.com>
2024-09-03 09:07:16 -0400

    fix param types

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -2,12 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import { useTranslation } from "../i18n";
 
-export default async function Home({ params: { lng } }) {
-  const { t } = await useTranslation(lng, "translation");
+interface HomePageProps {
+  params: {
+    lng: string;
+  };
+  someProp: string;
+}
+
+export default async function Home({ params, someProp }: HomePageProps) {
+  const { t } = await useTranslation(params.lng, "translation");
   return (
     <div>
       <h1>{t("title")}</h1>
-      <Link href={`/${lng}/second`}>{t("to-second-page")}</Link>
+      <Link href={`/${params.lng}/second`}>{t("to-second-page")}</Link>
     </div>
   );
 }
--- app/[lng]/second/page.tsx
+++ app/[lng]/second/page.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
 import { useTranslation } from "../../i18n";
 
-const SecondPage = async ({ params: { lng } }) => {
-  const { t } = await useTranslation(lng, "second-page");
+interface SecondPageProps {
+  params: {
+    lng: string;
+  };
+  fname: string;
+  isGreat: boolean;
+}
+const SecondPage = async ({ params, fname, isGreat }: SecondPageProps) => {
+  const { t } = await useTranslation(params.lng, "second-page");
   return (
     <>
       <h1>{t("title")}</h1>
-      <Link href={`/${lng}`}>{t("back-home")}</Link>
+      <Link href={`/${params.lng}`}>{t("back-home")}</Link>
     </>
   );
 };
